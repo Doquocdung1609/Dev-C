35189bd150a84003b2361889f0957165b13deca6
Doquocdung1609 <doquocdung1609@gmail.com>
2023-09-08 20:58:03 +0700

    Bai tap 8/9/2023

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int x;
+	int y;
+	float t1;
+	float n = 15.6;
+	char c = 'K';
+	int h1 = 5;
+	int h2 = 12;
+	int h = h1 + h2;
+	unsigned int k = 10; // so nguyen duong
+	short int kk = 6;
+	long int kkkk = 100; 
+	 
+	 printf("Xin chao");
+	 printf("Gia tri kk = %d, kk");
+	 printf("\nGia tri kk = %d, kk");
+	 
+	 
+	return 0;
+}
